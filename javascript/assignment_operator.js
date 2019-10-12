88979bc626ad3025fe5b008e4ed9b4ca6047cfb8
priyankapadwal <priyapadwal17@gmail.com>
2019-10-12 16:20:25 +0530

    assignment operator

--- javascript/assignment_operator.js
+++ javascript/assignment_operator.js
@@ -11,4 +11,13 @@ console.log("num="+num);
 console.log("num1="+num1);
 console.log("str="+str);
 
+var a=1;
+var b=10;
+a++;                           //a+1
+b--;                           //b-1
+console.log("a="+a);
+console.log("b="+b);
+
+
+
 
