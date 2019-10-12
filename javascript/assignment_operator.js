8bfb730fd6bb177c73655041af386cb76a054cd0
priyankapadwal <priyapadwal17@gmail.com>
2019-10-12 16:14:14 +0530

    javascript variables

---
+++ javascript/assignment_operator.js
@@ -0,0 +1,14 @@
+//storing variables with assignment operator
+
+var num= 5;
+var num1=10;
+var str= 'i am a ';
+
+num= num+1;
+num1=num-5;
+str =str+'string!';
+console.log("num="+num);
+console.log("num1="+num1);
+console.log("str="+str);
+
+
---
+++ javascript/variables.js
@@ -0,0 +1,19 @@
+//initialize variables value
+
+var a;
+console.log(a);
+var str='angular';
+var num=true;
+a=7;
+console.log(a);
+a=str;
+console.log(a);
+
+//scope of variables
+
+var b=10            //troughout the program
+let name='priya'    //only use within the scope where we declare that
+const pi=3.14       //constant value
+
+
+
