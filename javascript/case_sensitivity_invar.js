79abc3e9833ba2270e17576dd8b554655cfa3b2b
priyankapadwal <priyapadwal17@gmail.com>
2019-10-14 10:42:24 +0530

    case sensitivity

--- javascript/case_sensitivity_invar.js
+++ javascript/case_sensitivity_invar.js
@@ -1,9 +1,11 @@
 
-var  fIstNaMe;
-var  laStNaMe;
+var  fistName;
+var  lastName;
 
 FIRSTNAME='priyanka'
+fistName='priya'
 lAstNaMe='padwal'
 
 console.log(FIRSTNAME);  
+console.log(fistName);  
 console.log(lAstNaMe);
