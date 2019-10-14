ed532f133f4999a9e7748fd1e85eab3d73a8be6f
priyankapadwal <priyapadwal17@gmail.com>
2019-10-14 12:12:17 +0530

    string operations

--- javascript/string.js
+++ javascript/string.js
@@ -20,3 +20,21 @@ str1+=" to javascript";
 console.log("string="+str1);
 var str2=str1+" coderes.";
 console.log("string="+str2);
+
+//string length
+var lengthOfstr=0;
+var name='john';
+lengthOfstr=name.length;
+console.log("length of name="+lengthOfstr);
+
+//find character in string
+var firstLetter="";
+var firstName='joy';
+firstLetter=firstName[0];
+console.log("first letter of first name is ="+firstLetter);
+
+var lastLetter="";
+lastLetter=firstName[firstName.length-1];
+console.log("last letter of first name is ="+lastLetter);
+
+
