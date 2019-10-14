31497b0dbac12eb558d8037fde6deb4db71b76e8
priyankapadwal <priyapadwal17@gmail.com>
2019-10-14 10:57:16 +0530

    arithmatic operations

---
+++ javascript/arithatic_operations.js
@@ -0,0 +1,23 @@
+//remainder
+
+var remainder;
+remainder=11%3;
+console.log(remainder);
+
+//Compound assignment with Augmented additon,substraction, division,multiplication
+
+var a=10;
+a+=10;
+console.log("a="+a);
+
+var b20;
+b-=5;
+console.log("b="+b);
+
+var c=5;
+c*=5;
+console.log("c="+c);
+
+var d=12;
+d/=2;
+console.log("d="+d);
